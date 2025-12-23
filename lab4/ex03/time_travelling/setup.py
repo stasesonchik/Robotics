0ffb3bd459d874318bdf32192f33caaa84014c55
stasesonchik <144207492+stasesonchik@users.noreply.github.com>
2025-12-23 16:17:09 +0700

    Update setup.py

--- lab4/ex03/time_travelling/setup.py
+++ lab4/ex03/time_travelling/setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='yan',
+    maintainer='stas',
     maintainer_email='s.malyarchuk@g.nsu.ru',
     description='TODO: Package description',
     license='Apache-2.0',
